b4cb90fd6d3e3ad870053627f7ac25442a074e05
Alex Avlonitis <avlonitis@msn.com>
2016-02-16 17:06:52 +0000

    update

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,37 +4,38 @@ describe ('Airport', function(){
   var plane2 = new Plane();
   var plane3 = new Plane();
 
-  it ('lands a plane', function(){
+  beforeEach(function(){
     spyOn(airport1, "isWeather").and.returnValue(true);
+  });
+
+  it ('lands a plane', function(){
+
     airport1.land(plane1);
     expect(airport1.planes).toContain(plane1);
   });
 
   it ('lands a plane', function(){
-    spyOn(airport1, "isWeather").and.returnValue(true);
     airport1.land(plane2);
     expect(airport1.planes).toContain(plane2);
   });
 
   it ('prevents landing when plane capacity is reached', function(){
-    spyOn(airport1, "isWeather").and.returnValue(true);
     expect(airport1.land(plane3)).toEqual("Can't land, over capacity");
   });
 
   it ('prevents take off in bad weather', function(){
-    spyOn(airport1, "isWeather").and.returnValue(false);
+    airport1.isWeather.and.returnValue(false);
     expect(airport1.takeoff(plane1)).toEqual("Bad weather, can't take off");
   });
 
 
   it('makes the plane take off', function(){
-    spyOn(airport1, "isWeather").and.returnValue(true);
     airport1.takeoff(plane2);
     expect(airport1.planes).not.toContain(plane2);
   });
 
   it ('prevents landing in bad weather', function(){
-    spyOn(airport1, "isWeather").and.returnValue(false);
+    airport1.isWeather.and.returnValue(false);
     expect(airport1.land(plane1)).toEqual("Bad weather, can't land");
   });
 
